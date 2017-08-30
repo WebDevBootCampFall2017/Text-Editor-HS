7e247f33ed4a8005ba7589ebf2f759d9e80b73ec
HouSheng <houshengchiang@gmali.com>
2017-08-29 23:46:04 -0400

    find form

---
+++ Project 2 Text_Editor/Find.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Project_2_Text_Editor
+{
+    partial class frmFind
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.lblFind = new System.Windows.Forms.Label();
+            this.btnCancelFind = new System.Windows.Forms.Button();
+            this.lblReplace = new System.Windows.Forms.Label();
+            this.txtReplace = new System.Windows.Forms.TextBox();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFind
+            // 
+            this.txtFind.AcceptsTab = true;
+            this.txtFind.Location = new System.Drawing.Point(47, 12);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(186, 22);
+            this.txtFind.TabIndex = 0;
+            this.txtFind.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtFind_KeyDown);
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(239, 12);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(80, 23);
+            this.btnFind.TabIndex = 1;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // lblFind
+            // 
+            this.lblFind.AutoSize = true;
+            this.lblFind.Location = new System.Drawing.Point(12, 17);
+            this.lblFind.Name = "lblFind";
+            this.lblFind.Size = new System.Drawing.Size(29, 12);
+            this.lblFind.TabIndex = 2;
+            this.lblFind.Text = "Find:";
+            // 
+            // btnCancelFind
+            // 
+            this.btnCancelFind.Location = new System.Drawing.Point(325, 12);
+            this.btnCancelFind.Name = "btnCancelFind";
+            this.btnCancelFind.Size = new System.Drawing.Size(80, 23);
+            this.btnCancelFind.TabIndex = 3;
+            this.btnCancelFind.Text = "Cancel";
+            this.btnCancelFind.UseVisualStyleBackColor = true;
+            this.btnCancelFind.Click += new System.EventHandler(this.btnCancelFind_Click);
+            // 
+            // lblReplace
+            // 
+            this.lblReplace.AutoSize = true;
+            this.lblReplace.Location = new System.Drawing.Point(12, 45);
+            this.lblReplace.Name = "lblReplace";
+            this.lblReplace.Size = new System.Drawing.Size(68, 12);
+            this.lblReplace.TabIndex = 4;
+            this.lblReplace.Text = "Replace with:";
+            // 
+            // txtReplace
+            // 
+            this.txtReplace.Location = new System.Drawing.Point(86, 40);
+            this.txtReplace.Name = "txtReplace";
+            this.txtReplace.Size = new System.Drawing.Size(147, 22);
+            this.txtReplace.TabIndex = 5;
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(239, 40);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(166, 23);
+            this.btnReplace.TabIndex = 6;
+            this.btnReplace.Text = "Replace";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // frmFind
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 73);
+            this.Controls.Add(this.btnReplace);
+            this.Controls.Add(this.txtReplace);
+            this.Controls.Add(this.lblReplace);
+            this.Controls.Add(this.btnCancelFind);
+            this.Controls.Add(this.lblFind);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.txtFind);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(433, 124);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(433, 85);
+            this.Name = "frmFind";
+            this.ShowIcon = false;
+            this.Text = "Find";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFind;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Label lblFind;
+        private System.Windows.Forms.Button btnCancelFind;
+        public System.Windows.Forms.Label lblReplace;
+        public System.Windows.Forms.TextBox txtReplace;
+        public System.Windows.Forms.Button btnReplace;
+    }
+}
---
+++ Project 2 Text_Editor/Find.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Find size:433,85
+
+
+namespace Project_2_Text_Editor
+{
+    public partial class frmFind : Form
+    {
+        int TextIndex;
+        public frmFind()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            //retrieve text from the richtextbox in the Main form.
+            Form f = Application.OpenForms["frmTextEditor"];
+            ((frmTextEditor)f).rtxRichTextBox.Visible = false;
+            string textfromTE = ((frmTextEditor)f).rtxRichTextBox.Text;
+            string TextToFind = txtFind.Text;
+          
+
+            TextIndex = textfromTE.IndexOf(TextToFind);
+
+            if (TextIndex <0)
+            {
+                MessageBox.Show("Can't Find the text: " + TextToFind);
+            }
+            else if(TextToFind.Length == 0)
+            {
+                MessageBox.Show("Please Enter a text");
+            }
+            else
+            {
+                ((frmTextEditor)f).rtxRichTextBox.SelectionStart = TextIndex;
+                ((frmTextEditor)f).rtxRichTextBox.SelectionLength = TextToFind.Length;
+                this.Close();
+            }
+        }
+
+
+        private void btnCancelFind_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtFind_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnFind_Click(this, new EventArgs());
+            }
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
